56f1c9de50ba6b3370d13cc71e2c80891d10aadc
Rishabh Poddar <rishabhp89@gmail.com>
2020-02-06 20:22:54 +0000

    Add TLS support using mbedTLS

---
+++ enclave/rabit/src/ssl_socket.cc
@@ -0,0 +1,162 @@
+#include "ssl_socket.h"
+#include "../include/dmlc/logging.h"
+#include "ssl_context_manager.h"
+
+namespace rabit {
+namespace utils {
+
+namespace {}  // namespace
+bool SSLTcpSocket::SSLConnect(const SockAddr &addr) {
+  //mbedtls_net_init(&net);
+  mbedtls_ssl_config_init(&conf);
+  mbedtls_x509_crt_init(&cacert);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  // seeds and sets up entropy source
+  mbedtls_entropy_init(&entropy);
+  int ret;
+  if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0)) != 0) {
+    print_err(ret);
+  }
+
+  if (Connect(addr)) {
+    mbedtls_ssl_init(&ssl_);
+
+    // configure TLS layer
+    if ((ret = mbedtls_ssl_config_defaults(&conf,
+        MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT)) != 0) {
+      print_err(ret);
+    }
+    // FIXME add certificate auth (currently not verifying) 
+    mbedtls_ssl_conf_authmode(&conf, MBEDTLS_SSL_VERIFY_NONE);
+    mbedtls_ssl_conf_ca_chain(&conf, &cacert, NULL);
+    // configure RNG
+    mbedtls_ssl_conf_rng(&conf, mbedtls_ctr_drbg_random, &ctr_drbg);
+    // set up SSL context
+    if ((ret = mbedtls_ssl_setup(ssl(), &conf)) != 0) {
+      print_err(ret);
+    }
+    // configure hostname
+    // FIXME set hostname to check against cert
+    //if ((ret = mbedtls_ssl_set_hostname(&ssl, addr.AddrStr().c_str())) != 0) {
+      //print_err(ret);
+    //  Error("Error: Could not set hostname");
+    //}
+
+    // configure input/output functions for sending data
+    mbedtls_net_init(&net);
+    net.fd = this->sockfd;
+    mbedtls_ssl_set_bio(ssl(), &net, mbedtls_net_send, mbedtls_net_recv, NULL);
+
+    // perform handshake
+    while ((ret = mbedtls_ssl_handshake(ssl())) != 0) {
+      if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE) {
+        print_err(ret);
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+SSLTcpSocket SSLTcpSocket::SSLAccept() {
+  SOCKET client_fd = Accept();
+
+  SSLTcpSocket client_sock(client_fd);
+  mbedtls_ssl_config_init(&client_sock.conf);
+  int ret;
+
+  mbedtls_x509_crt srvcert;
+  mbedtls_x509_crt cachain;
+  mbedtls_pk_context pkey;
+
+  mbedtls_x509_crt_init(&srvcert);
+  mbedtls_x509_crt_init(&cachain);
+  mbedtls_ctr_drbg_init(&ctr_drbg);
+  // seeds and sets up entropy source
+  mbedtls_entropy_init(&entropy);
+  if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, NULL, 0)) != 0) {
+    print_err(ret);
+  }
+
+  // FIXME currently using inbuilt certs / key
+  ret = mbedtls_x509_crt_parse( &srvcert, (const unsigned char *) mbedtls_test_srv_crt_ec, mbedtls_test_srv_crt_ec_len );
+  if( ret != 0 ) {
+    print_err(ret);
+  }
+  ret = mbedtls_x509_crt_parse( &cachain, (const unsigned char *) mbedtls_test_cas_pem, mbedtls_test_cas_pem_len );
+  if( ret != 0 ) {
+    print_err(ret);
+  }
+  mbedtls_pk_init( &pkey );
+  ret =  mbedtls_pk_parse_key( &pkey, (const unsigned char *) mbedtls_test_srv_key_ec, mbedtls_test_srv_key_ec_len, NULL, 0 );
+  if( ret != 0 ) {
+    print_err(ret);
+  }
+  mbedtls_ssl_conf_ca_chain( &client_sock.conf, &cachain, NULL );
+  if( ( ret = mbedtls_ssl_conf_own_cert( &client_sock.conf, &srvcert, &pkey ) ) != 0 ) {
+    print_err(ret);
+  }
+
+  if( ( ret = mbedtls_ssl_config_defaults( &client_sock.conf,
+          MBEDTLS_SSL_IS_SERVER,
+          MBEDTLS_SSL_TRANSPORT_STREAM,
+          MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 ) {
+    print_err(ret);
+  }
+
+  mbedtls_ssl_conf_rng( &client_sock.conf, mbedtls_ctr_drbg_random, &ctr_drbg );
+  // Make sure memory refs are valid
+  mbedtls_ssl_init( &client_sock.ssl_);
+
+  if ((ret = mbedtls_ssl_setup(&client_sock.ssl_, &client_sock.conf)) != 0) {
+    print_err(ret);
+  }
+
+  mbedtls_net_init(&client_sock.net);
+  client_sock.net.fd = client_sock.sockfd;
+  mbedtls_ssl_set_bio(&client_sock.ssl_, &client_sock.net,  mbedtls_net_send, mbedtls_net_recv, NULL);
+
+  while( ( ret = mbedtls_ssl_handshake(&client_sock.ssl_ ) ) != 0 ) {
+    if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE ) {
+      print_err(ret);
+    }
+  }
+  return client_sock;
+}
+
+size_t SSLTcpSocket::SSLSendAll(const void *buf_, size_t len) {
+  const char *buf = reinterpret_cast<const char *>(buf_);
+  size_t ndone = 0;
+  while (ndone < len) {
+    ssize_t ret = SSLSend(buf, static_cast<ssize_t>(len - ndone));
+    if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE && ret < 0) {
+      if (LastErrorWouldBlock())
+        return ndone;
+      print_err(ret);
+    }
+    buf += ret;
+    ndone += ret;
+  }
+  return ndone;
+}
+
+size_t SSLTcpSocket::SSLRecvAll(void *buf_, size_t len) {
+  char *buf = reinterpret_cast<char *>(buf_);
+  size_t ndone = 0;
+  while (ndone < len) {
+    ssize_t ret = SSLRecv(buf, static_cast<sock_size_t>(len - ndone));
+    if (ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE && ret < 0) {
+      if (LastErrorWouldBlock())
+        return ndone;
+      print_err(ret);
+    }
+    if (ret == 0) return ndone;
+    buf += ret;
+    ndone += ret;
+  }
+  return ndone;
+}
+
+} /* namespace utils */
+} /* namespace rabit */
---
+++ enclave/rabit/src/ssl_socket.h
@@ -0,0 +1,159 @@
+#ifndef RABIT_TLS_SOCKET_H_
+#define RABIT_TLS_SOCKET_H_
+
+#include "socket.h"
+#include "ssl_context_manager.h"
+
+namespace rabit {
+namespace utils {
+
+static void print_err(int error_code) {
+  const size_t LEN = 2048;
+  char err_buf[LEN];
+  mbedtls_strerror(error_code, err_buf, LEN);
+  mbedtls_printf(" ERROR %d: %s\n", getpid(), err_buf);
+  exit(1);
+}
+
+class SSLTcpSocket : public TCPSocket {
+ public:
+  //using SharedSSL = std::shared_ptr<mbedtls_ssl_context>;
+  using SharedSSL = mbedtls_ssl_context;
+
+  // unique_ptr deleter got called only get() != nullptr.
+  SSLTcpSocket() : TCPSocket(), ssl_() {}
+
+  SSLTcpSocket(SOCKET sockfd) : TCPSocket(sockfd), ssl_() {}
+
+  SSLTcpSocket(SOCKET sockfd, SharedSSL ssl) : TCPSocket(sockfd), ssl_(ssl) {}
+
+  // Indicate has ssl context.
+  //bool HasSSL() const { return ssl_.get() != nullptr; }
+
+  void SetSSL(SharedSSL ssl) {
+    //if (HasSSL()) {
+    //  Socket::Error("Already has ssl.");
+    //}
+    ssl_ = ssl;
+  }
+
+  SSLTcpSocket* getPtr() {
+    return this;
+  }
+
+  // SSL Accept.
+  SSLTcpSocket SSLAccept();
+
+  // SSL Connect.
+  bool SSLConnect(const SockAddr &addr);
+
+  void setBio() {
+    mbedtls_net_init(&net);
+    net.fd = this->sockfd;
+    mbedtls_ssl_set_bio(ssl(), &net, mbedtls_net_send, mbedtls_net_recv, NULL);
+  }
+
+  // SSL Write, note this does not support |flag| argument.
+  ssize_t SSLSend(const void *buf, size_t len) {
+    ssize_t ret = -1;
+    while( ( ret = mbedtls_ssl_write( ssl(), (const unsigned char*)buf, len ) ) <= 0 )
+    {
+        if( ret == MBEDTLS_ERR_NET_CONN_RESET )
+        {
+          print_err(ret);
+          break;
+        }
+
+        if( ret != MBEDTLS_ERR_SSL_WANT_READ && ret != MBEDTLS_ERR_SSL_WANT_WRITE )
+        {
+          print_err(ret);
+          break;
+        }
+    }
+    return ret;
+  }
+
+  // SSL Read, note this does not support |flag| argument.
+  ssize_t SSLRecv(void *buf, size_t len) { 
+    ssize_t ret = -1;
+    do {
+        ret = mbedtls_ssl_read(ssl(), (unsigned char*)buf, len);
+
+        if( ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE )
+            continue;
+
+        if( ret <= 0 )
+        {
+            switch( ret )
+            {
+                case MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY:
+                  print_err(ret);
+                  return -1;
+
+                case MBEDTLS_ERR_NET_CONN_RESET:
+                  print_err(ret);
+                  return -1;
+
+                default:
+                  print_err(ret);
+                  return -1;
+            }
+        }
+
+        if( ret > 0 )
+            break;
+    } while( 1 );
+    return ret;
+  }
+
+  // SSL SendAll
+  size_t SSLSendAll(const void *buf_, size_t len);
+
+  // SSL RecvAll
+  size_t SSLRecvAll(void *buf_, size_t len);
+
+  /*!
+   * \brief send a string over network
+   * \param str the string to be sent
+   */
+  void SSLSendStr(const std::string &str) {
+    int len = static_cast<int>(str.length());
+    utils::Assert(this->SSLSendAll(&len, sizeof(len)) == sizeof(len),
+                  "error during send SendStr");
+    if (len != 0) {
+      utils::Assert(this->SSLSendAll(str.c_str(), str.length()) == str.length(),
+                    "error during send SendStr");
+    }
+  }
+
+  /*!
+   * \brief recv a string from network
+   * \param out_str the string to receive
+   */
+  void SSLRecvStr(std::string *out_str) {
+    int len;
+    utils::Assert(this->SSLRecvAll(&len, sizeof(len)) == sizeof(len),
+                  "error during send RecvStr");
+    out_str->resize(len);
+    if (len != 0) {
+      utils::Assert(this->SSLRecvAll(&(*out_str)[0], len) == out_str->length(),
+                    "error during send SendStr");
+    }
+  }
+
+  mbedtls_ssl_context *ssl() { return &ssl_; }
+  mbedtls_ssl_config *conf_() { return &conf; }
+ private:
+  SharedSSL ssl_;
+
+  mbedtls_net_context net;
+  mbedtls_ctr_drbg_context ctr_drbg;
+  mbedtls_ssl_config conf;
+  mbedtls_entropy_context entropy;
+  mbedtls_x509_crt cacert;
+};
+
+}  // namespace utils
+}  // namespace rabit
+
+#endif  // RABIT_TLS_SOCKET_H_
