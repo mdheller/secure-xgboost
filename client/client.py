f19e71fea442b5d4bda709f1f4fa612c5261b5c4
Chester Leung <chester.leung@berkeley.edu>
2020-02-07 05:48:08 +0000

    Python client crypto functions!

--- client/client.py
+++ client/client.py
@@ -11,4 +11,9 @@ def generate_client_key(path_to_key):
         keyfile.write(key)
 
 def encrypt_file(input_path, output_path, client_key):
+    print("Encrypting file {}".format(input_path))
     call('./encrypt-file ' + input_path + ' ' + output_path + " " + client_key, shell=True)
+
+def decrypt_file(input_path, output_path, client_key):
+    print("Decrypting file {}".format(input_path))
+    call('./decrypt-file ' + input_path + ' ' + output_path + " " + client_key, shell=True)
--- client/demo.py
+++ client/demo.py
@@ -1,3 +1,12 @@
 from client import *
 
-generate_client_key("key.txt")
+KEY_FILE = "key.txt"
+
+# First generate a key that you will use to encrypt your data
+generate_client_key(KEY_FILE)
+
+# Next use the generated key to encrypt data
+encrypt_file("../demo/data/agaricus.txt.train", "train.enc", KEY_FILE)
+
+# Sanity check: decrypt the encrypted file and see if it matches the original
+decrypt_file("train.enc", "train.ptxt", KEY_FILE)
